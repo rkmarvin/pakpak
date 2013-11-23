f0d1bb3fcc3c3fae921ae0bb3a323dc89538705b
marvin <rouslan.korkmazov@gmail.ru>
2013-11-23 17:33:00 +0400

    test

--- pakpak/static/js/utils.js
+++ pakpak/static/js/utils.js
@@ -15,7 +15,18 @@ function init()
                 i = 0;
             }
         }, false);
-    }
+    };
+
+    $('.song').on('click', function(e){
+        var src = $(e.target).attr('href');
+        var audio = $('<audio controls="controls" align="" autoload/>').attr('src', src)
+        
+        $('#' + $(e.targer).id).append(audio);
+
+        console.log(src);
+        console.log(audio);
+        console.log('done');
+    });
     audio_setup();
 };
 
